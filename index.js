e746fa7eddd04667b86a4aa363e2640902bb4b91
Emily <aspentree79@gmail.com>
2019-06-09 13:16:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // write your code below!
+function happyHolidays () {
+  var greeting = "Happy holidays!";
+return greeting
+}
+
+function happyHolidaysTo (name) {
+  console.log(`Happy holidays, you!`)
+return `Happy holidays, ${name}!`
+}
+
+function happyHolidaysTo (holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown (holiday, days) {
+  return `It\'s ${days} days until ${holiday}!`
+}
